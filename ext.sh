a0753f73fb69e02753538dd72e0a6c726eedb940
Steven Burgess <sburgess@datto.com>
2015-12-23 15:17:20 -0500

    Remove commented out lines

--- ext.sh
+++ ext.sh
@@ -9,7 +9,6 @@ truncate -s 3g /problem/fs/file
 device=$(losetup -f --show /problem/fs/file)
 
 zfs snapshot problem/fs@base
-# write random data to the end
 mkfs.ext4 -E discard $device
 mkdir /tmp/zfsprobext
 mount -o discard $device /tmp/zfsprobext
@@ -33,14 +32,9 @@ zfs send -i 3 problem/fs@end | zfs recv problem/fs3
 zfs clone -o readonly=on problem/fs@end problem/clone1
 zfs clone -o readonly=on problem/fs3@end problem/clone3
 
-# while these checksums are interesting, its really diffs in the clones that
-# we are intrested in.
-#md5sum /problem/fs/file
-#md5sum /problem/fs2/file
 md5sum /problem/clone1/file
 md5sum /problem/clone3/file
 
 umount /tmp/zfsprobext
 losetup -d $device
-#rm /tmp/zfsprob/file
 rm -rf /tmp/zfsprobext
